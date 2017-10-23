232dd42ad9bb857c293efeb07117379abde14ac4
Antoine Renault <antoine.renault.mmi@gmail.com>
2017-10-23 14:49:34 +0200

    4.1.2 - Templates HTML
    Use promise on sass generation
    Use iframe to display HTML and add custom styling
    Add folder for data & add wip state of templates
    Add new SCSS to support show state
    Refactor to support correct variable & add interaction with new view
    Add templates page

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,6 @@
 'use strict'
 
-angular
+window.angular
   .module('bootstrapVariablesEditor', [
     'bootstrapVariablesEditor.filters',
     'bootstrapVariablesEditor.services',
@@ -10,15 +10,9 @@ angular
     'ui.codemirror'
   ])
   .config(function($routeProvider, $locationProvider) {
-    $locationProvider.hashPrefix('!')
-    $routeProvider.when('/editor', { templateUrl: 'partials/editor.html', controller: SassCtrl })
-    $routeProvider.when('/create-website-pikock', {
-      templateUrl: 'partials/create-website-pikock.html',
-      controller: PageCtrl
-    })
-    $routeProvider.when('/submit-theme', {
-      templateUrl: 'partials/submit-theme.html',
-      controller: PageCtrl
+    $routeProvider.when('/editor', {
+      templateUrl: 'partials/editor.html',
+      controller: window.SassCtrl
     })
     $routeProvider.otherwise({ redirectTo: '/editor' })
   })
